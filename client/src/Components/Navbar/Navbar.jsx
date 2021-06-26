b1ce2d9a44ed5231507a8afdb66417831b3457ba
Vedansh Wani <me.vedanshwani@gmail.com>
2021-06-26 15:58:28 +0530

    navbar ui

---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img className="topImg" src="" alt="img" />
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
+    </div>
+  );
+};
