cd74c7a22d764d91fbc265c86a7f958f3df8f7d4
Vedansh Wani <me.vedanshwani@gmail.com>
2021-06-30 22:03:31 +0530

    implemented multer

--- server/index.js
+++ server/index.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const multer = require("multer");
 const authController = require("./controllers/auth");
 const userController = require("./controllers/users");
 const postController = require("./controllers/post");
 const categoryController = require("./controllers/category");
 dotenv.config();
 const app = express();
-
 app.use(express.json());
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.status(201).json({ msg: "File has been uploaded" });
+});
+
 app.use("/auth", authController);
 app.use("/user", userController);
 app.use("/post", postController);
